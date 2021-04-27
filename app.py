3cefcc7031ad74ec61fda8f707460ca6d62cc70e
AmyWagarCinch <gretylschmetyl@gmail.com>
2021-04-27 14:21:16 -0400

    playing with saving model, moving model into flask app

--- app.py
+++ app.py
@@ -1,13 +1,25 @@
 from flask import Flask
 from flask import request
 from flask import render_template
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+import pickle
 
 app = Flask(__name__)
+
+## load the model from disk
+#loaded_model = pickle.load(open('homepriceModel_trained.sav', 'rb'))
+#result = loaded_model.score(X_test, y_test)
+#Check the pickle file by inputing the variables
+model = pickle.load(open('trained_model.pkl','rb'))
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    #result = loaded_model.score(X_test, y_test)
+    #print(result)
+    result = model.predict([[55, 18, 0, 1, 1]])
+    return render_template('home.html', prediction_text='Apparently this worked: $ {}'.format(result))
 
 @app.route("/about")
 def about():
